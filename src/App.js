eccad61c483e9a23d339e47e994d44ef0032b091
jellev00 <jelle.vandriessche@gmail.com>
2023-09-17 22:53:51 +0200

    code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!!!!</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
